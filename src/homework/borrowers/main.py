fa382da5c105a47adf0dabcfd5ae1db04c4bd8ca
AlbertoNuno <anunoguevara@gmail.com>
2020-10-12 19:21:32 -0500

    Borrowers homework implementation

--- src/homework/borrowers/main.py
+++ src/homework/borrowers/main.py
@@ -1,7 +1,7 @@
 from .models import Borrower
-
 from fintools.settings import get_logger
-
+import json
+from .utils import get_current_utc
 logger = get_logger(name="__main__")
 DEFAULT_FILENAME = "./borrowers/candidates.json"
 
@@ -11,13 +11,18 @@ class Main:
     @staticmethod
     def show(file: str = DEFAULT_FILENAME) -> str:
         logger.info("Calling the show method.")
-        # TODO: read file and show content
-        pass
+        with open (file,"r") as f:
+            data = json.load(file)
+        return json.dumps(data)
 
     @staticmethod
     def insert(email: str, age: int, income: float, file: str = DEFAULT_FILENAME):
+
         borrower = Borrower(email=email, age=age, income=income)
-        borrower.save(file=file)
+        new_file = borrower.save(file=file)
+        return new_file
+
+
 
     @staticmethod
     def update(email: str, age: int, income: float, file: str = DEFAULT_FILENAME):
