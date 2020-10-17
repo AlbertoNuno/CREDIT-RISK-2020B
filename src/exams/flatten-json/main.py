eb422319f39260238bc03476a55a6e4e44417534
AlbertoNuno <anunoguevara@gmail.com>
2020-10-17 10:26:38 -0500

    Exam changes

--- src/exams/flatten-json/main.py
+++ src/exams/flatten-json/main.py
@@ -5,12 +5,12 @@ from .utils import *
 class Main:
 
     @staticmethod
-    def show(file: str = FILE_NAME) -> str:
+    def show(file: str) -> str:
         with open(file, "r") as f:
             data = json.load(f)
         return json.dumps(data)
     @staticmethod
-    def flatten(file: str = FILE_NAME) -> str:
+    def flatten(file: str) -> str:
         flattened_file = to_dict(file)
         new_file = flatten_dict(flattened_file)
         return new_file
