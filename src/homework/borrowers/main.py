3d021458996dbc6bb0b22a8061b613cc716b6b9d
AlbertoNuno <anunoguevara@gmail.com>
2020-10-17 10:31:00 -0500

    Changes for borrowers homework

--- src/homework/borrowers/main.py
+++ src/homework/borrowers/main.py
@@ -1,18 +1,19 @@
 from .models import Borrower
-
+import json
 from fintools.settings import get_logger
 
 logger = get_logger(name="__main__")
-DEFAULT_FILENAME = "./borrowers/candidates.json"
+DEFAULT_FILENAME = "C:/Users/anuno/OneDrive/Documents/ITESO/Modelos de crédito/CREDIT-RISK-2020B/src/homework/borrowers/candidates.json"
 
 
 class Main:
 
     @staticmethod
     def show(file: str = DEFAULT_FILENAME) -> str:
-        logger.info("Calling the show method.")
-        # TODO: read file and show content
-        pass
+        # logger.info("Calling the show method.")
+        with open(file, "r") as f:
+            data = json.load(f)
+        return json.dumps(data)
 
     @staticmethod
     def insert(email: str, age: int, income: float, file: str = DEFAULT_FILENAME):
--- src/homework/borrowers/models.py
+++ src/homework/borrowers/models.py
@@ -14,13 +14,39 @@ class Borrower:
         self.income = income
 
     def to_json(self) -> Dict:
-        # TODO: return a json with the: email, age, income, created_at, and updated_at
-        pass
+        #        out_dict = {'upddated_at':self.updated_at,
+        #                      'candidates':[]
+        #                    }
+        out_dict = {"email": self.email,
+                    "age": self.age,
+                    "income": self.income,
+                    "created_at": self.created_at,
+                    "updated_at": self.updated_at
+                    }
+
+        return out_dict
 
     def save(self, file: str):
-        # TODO: save the borrower into the json file!
-        pass
+        data = self.to_json()
+        with open(file, "r") as f:
+            borrowers_file = json.load(f)
+        borrowers_file["updated_at"] = get_current_utc()
+        borrowers_file["candidates"].append(data)
+        with open(file, 'w') as file:
+            file.write(json.dumps(borrowers_file, indent=4))
 
     def update(self, file: str):
-        # TODO: update the borrower on the json file that match the email of the current borrower.
-        pass
+        data = self.to_json()
+        with open(file, "r")as f:
+            borrowers_file = json.load(f)
+        borrowers_file["updated_at"] = get_current_utc()
+        for i in borrowers_file["candidates"]:
+            if self.email == i["email"]:
+                i["age"] = self.age
+                i["income"] = self.income
+                i["updated_at"] = get_current_utc()
+        with open(file, "w") as file:
+            file.write(json.dumps(borrowers_file, indent=4))
+
+
+
