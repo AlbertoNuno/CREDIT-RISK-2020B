b23a2379b651ff256c852e77760d86684184d848
Alberto Nuño <anunoguevara@gmail.com>
2020-11-17 20:03:05 -0600

    New changes to main.py

--- src/homework/amortization/main.py
+++ src/homework/amortization/main.py
@@ -1,18 +1,27 @@
 from typing import Optional
-
 from fintools import Amortization
+import pandas as pd
+import amortization as am
+a= pd.DataFrame()
 
 
 class Main:
 
     @staticmethod
     def annuity(amount: float, rate: float, n: int):
-        raise NotImplementedError
+        ann = am.Amortization(amount, rate, n)
+        return ann.annuity
 
     @staticmethod
     def table(amount: float, rate: float, n: int, save_file: Optional[str] = None):
-        raise NotImplementedError
+        ann = am.Amortization(amount, rate, n)
+        return ann.get_table()
 
     @staticmethod
     def plot(amount: float, rate: float, n: int, save_file: Optional[str] = None):
+        to_plot = am.Amortization(amount, rate, n)
+        return to_plot.plot()
+
+
+n: int, save_file: Optional[str] = None):
         raise NotImplementedError
